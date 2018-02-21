c7bbfa5c5e94b1eb7dbc3a45d0dec5d88b68efc1
Menno Dekker <menno.dekker@erasmusmc.nl>
2018-02-21 14:18:51 +0100

    Testcase for #2

--- test/src/HL7/Extractor/RespondentExtractorTest.php
+++ test/src/HL7/Extractor/RespondentExtractorTest.php
@@ -69,6 +69,36 @@ class RespondentExtractorTest extends RespondentExtractorTestAbstract
                 $expectedResult, $this->respondentExtractor->extractRow($message)
         );
     }
+    
+    public function testRespondent3()
+    {
+        $message = $this->_getMessageFromFile(TEST_DIR . '/resources/orm_2.txt');
+
+        $expectedResult = array(
+            'gr2o_patient_nr'      => '1001303',
+            'gr2o_id_organization' => 'NES',
+            'gr2o_reception_code'  => 'OK',
+            'grs_ssn'              => '134960713',
+            'grs_iso_lang'         => 'DE',
+            'grs_first_name'       => 'Klaas',
+            'grs_surname_prefix'   => 'de',
+            'grs_last_name'        => 'Vries-Peterse',
+            'grs_gender'           => 'F',
+            'grs_birthday'         => '1970-01-15',
+            'grs_address_1'        => 'Woonstraße 2232 II',
+            'grs_zipcode'          => '90210',
+            'grs_city'             => 'Kukeleku',
+            'grs_iso_country'      => 'DE',
+            'grs_phone_1'          => '0106565656',
+            'grs_email'            => 'email@example.net',
+            'grs_phone_2'          => '0612345678',
+        );
+
+        $this->respondentExtractor->setSsnAutority('NLMINBIZA');
+        $this->assertEquals(
+                $expectedResult, $this->respondentExtractor->extractRow($message)
+        );
+    }
 
     public function testRespondentIdFailAuthority()
     {
