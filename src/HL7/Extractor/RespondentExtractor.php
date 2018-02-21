46be570c7c34ca391549337ddd5a95b2236f93c9
Menno Dekker <menno.dekker@erasmusmc.nl>
2018-02-21 14:20:46 +0100

    Fixed #2

--- src/HL7/Extractor/RespondentExtractor.php
+++ src/HL7/Extractor/RespondentExtractor.php
@@ -270,11 +270,11 @@ class RespondentExtractor implements ExtractorInterface
                         break;
 
                     case 'NL3':
-                        $lastName = $nameParts->getPartnerName() . '-' . join(' ', array($nameParts->getBirthPrefix(), $nameParts->getBirthName()));
+                        $lastName = $nameParts->getPartnerName() . '-' . join(' ', array_filter(array($nameParts->getBirthPrefix(), $nameParts->getBirthName())));
                         break;
 
                     case 'NL4':
-                        $lastName = $nameParts->getBirthName() . '-' . join(' ', array($nameParts->getPartnerPrefix(), $nameParts->getPartnerName()));
+                        $lastName = $nameParts->getBirthName() . '-' . join(' ', array_filter(array($nameParts->getPartnerPrefix(), $nameParts->getPartnerName())));
                         break;
 
                     case 'NL1':
